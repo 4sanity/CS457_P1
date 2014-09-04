0e84eec1220a54e824249e6f73840d82a9f671cf
Tragdor <sam.bruns0@gmail.com>
2014-09-03 19:26:31 -0600

    added server bind,listen

--- chat.cpp
+++ chat.cpp
@@ -3,16 +3,65 @@
 //Date: 9/3/2014
 //chat.cpp 
 
+#include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#define MAXPENDING 5
 
 using namespace std;
+
+struct In_addr {
+    unsigned long s_addr;
+};
+
+struct socketAddress{
+	unsigned short family;
+	unsigned short port;
+	struct In_addr addr;
+	char zero[8];
+};
 
 int main(int argc, char *argv[]){
 	string welcome = "Welcome to Chat!";
 	
+	int serverSocket;
+	struct socketAddress serverAddress;
+	struct socketAddress clientAddress;
+	int newSocket;
+	
 	if(argc==1){
 		//server side
+		if((serverSocket=socket(PF_INET,SOCK_STREAM,0))<0){
+			cout << "Error: Server socket could not be created." << endl;
+			exit(1);
+		}
+		
+		serverAddress.family=AF_INET;
+		serverAddress.addr.s_addr=htonl(INADDR_ANY);
+		serverAddress.port=htons(3400);
+		cout << serverAddress.addr.s_addr << "   " << serverAddress.port << endl;
+		if(bind(serverSocket,(struct sockaddr*)&serverAddress,sizeof(serverAddress))<0){
+			cout << "Error: Server failed to bind." << endl;
+			exit(1);
+		}
+		
+		if(listen(serverSocket, MAXPENDING)<0){
+			cout << "Error: Server failed to start listen." << endl;
+			exit(1);
+		}
+		
+		//not sure why socklen_t works
+		socklen_t clientLength = sizeof(clientAddress);
+		if((newSocket=accept(serverSocket,(struct sockaddr*)&clientAddress,&clientLength))<0){
+			cout << "Error: Server failed to accept connection." << endl;
+			exit(1);
+		}
+		
+		cout << welcome << "Waiting for a connection on " << serverAddress.addr.s_addr
+			<< " port " << serverAddress.port << endl;
 		
 		
 	}else if(argc==2){
