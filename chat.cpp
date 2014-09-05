38c0f2e606a24c1ffd76bea5bec3e8fff32bc0f6
Tragdor <sam.bruns0@gmail.com>
2014-09-04 18:34:59 -0600

    started on send message

--- chat.cpp
+++ chat.cpp
@@ -10,12 +10,12 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+
 #define MAXPENDING 5
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-	string welcome = "Welcome to Chat!";
 	
 	int serverSocket;
 	struct sockaddr_in serverAddress;
@@ -24,7 +24,6 @@ int main(int argc, char *argv[]){
 	
 	if(argc==1){
 		//server side
-		//when change IPPROTO_TCP from 0 fails to bind when i use the same port within a certain time
 		if((serverSocket=socket(PF_INET,SOCK_STREAM,0))<0){
 			cout << "Error: Server socket could not be created." << endl;
 			exit(1);
@@ -75,9 +74,10 @@ int main(int argc, char *argv[]){
 		string arg2 = argv[2];
 		string arg3 = argv[3];
 		string arg4 = argv[4];
-		int port;
-		string Saddress;
+		int PORT;
+		string SADDRESS;
 		
+		//make sure both arguments are not the same
 		if( (arg1=="-p"&&arg3=="-p") || (arg1=="-s"&&arg3=="-s") ){
 			cout << "Error: Arguments incorrect. Use ./chat -h for help message." << endl;
 			exit(1);
@@ -98,17 +98,56 @@ int main(int argc, char *argv[]){
 			cout << "Error: Could not convert port argument to number." << endl;
 			exit(1);
 		}
-		if(arg1=="-s" && !atoi(argv[2])){
-			cout << "Error: Could not convert port argument to number." << endl;
-			exit(1);
-		}		
-		if(arg3=="-s" && !atoi(argv[4])){
-			cout << "Error: Could not convert port argument to number." << endl;
+		if(arg1=="-s"){
+			for(unsigned i=0; i<arg2.length(); i++){
+				if( !isdigit(arg2[i]) && arg2[i]!='.' ){
+					cout << "Error: Server address is not in the correct format." << endl;
+					exit(1);
+				}
+			}
+			//if the server address comes first this is where i initialize
+			SADDRESS = arg2;
+			PORT = atoi(argv[4]);
+		}
+		if(arg3=="-s"){
+			for(unsigned i=0; i<arg4.length(); i++){
+				if( !isdigit(arg4[i]) && arg4[i]!='.'){
+					cout << "Error: Server address is not in the correct format." << endl;
+					exit(1);
+				}
+			}
+			//if the server address comes second this is where i initialize
+			SADDRESS = arg4;
+			PORT = atoi(argv[2]);
+		}
+		
+		//sanitization done
+		int clientSocket;
+		struct sockaddr_in serverAddress;
+
+		if((clientSocket=socket(PF_INET,SOCK_STREAM,0))<0){
+			cout << "Error: Client socket could not be created." << endl;
 			exit(1);
 		}
 		
+		serverAddress.sin_family = AF_INET;
+		serverAddress.sin_addr.s_addr = htonl(inet_network(SADDRESS.c_str()));  //inet_addr(SADDRESS)
+		serverAddress.sin_port = htons(PORT);
+
+   		if((connect(clientSocket,(struct sockaddr *)&serverAddress,sizeof(sockaddr_in))< 0)){
+			cout << "Error: Failed to connect with server." << endl;
+			exit(1);
+		}
+		cout << "Connecting to server... Connected!" << endl;
+		cout << "Connected to a friend! You send first." << endl;
+		cout << "You: ";
 		
-		
+		string message;
+		cin >> message;
+		if(message.length()>140){
+			cout << "Error: Input too long." << endl;
+			//ask for input?
+		}
 		
 	}else{
 		cout << "Error: Number of arguments incorrect. Use ./chat -h for help message." << endl;
@@ -116,3 +155,19 @@ int main(int argc, char *argv[]){
 
 return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
